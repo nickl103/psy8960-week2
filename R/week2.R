23f43861b65b95d2fb7e700a3d40b9b67d5684d7
nickl103 <nickl103@umn.edu>
2024-01-28 15:16:25 -0600

    analysis complete

--- R/week2.R
+++ R/week2.R
@@ -2,3 +2,10 @@
 rt_df <- read.csv("data/week2.csv", header=T)
 rt_df$condition <- factor(rt_df$condition, order=T, levels = c("A","B"), labels= c("Control", "Experimental"))
 rt_df$gender <- factor(rt_df$gender, order=T, levels= c("M", "F", "N"), labels= c("Male", "Female", "Nonbinary"))
+
+#Analysis
+mean(rt_df$rt)
+rt_f_df <-subset(rt_df, subset= gender =="Female")
+hist(rt_f_df$rt)
+datasets <- list(rt_df, rt_f_df)
+datasets[[1]][[2]]
