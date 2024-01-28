bda72a7ae40854324bf734d38ca0c9324ced95df
nickl103 <nickl103@umn.edu>
2024-01-28 15:15:06 -0600

    labeling complete

--- R/week2.R
+++ R/week2.R
@@ -1,2 +1,4 @@
 #Import and Labeling
 rt_df <- read.csv("data/week2.csv", header=T)
+rt_df$condition <- factor(rt_df$condition, order=T, levels = c("A","B"), labels= c("Control", "Experimental"))
+rt_df$gender <- factor(rt_df$gender, order=T, levels= c("M", "F", "N"), labels= c("Male", "Female", "Nonbinary"))
